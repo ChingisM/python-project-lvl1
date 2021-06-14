534e08dcb3c614f5a287d32c8364af28a72e6c56
Chingis Mandzhiev <chingis.mandzhievvv@gmail.com>
2021-06-14 22:39:47 +0300

    Correct function name

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,14 +4,14 @@ import random
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def define_gcd(num1, num2):
+def calculate_gcd(num1, num2):
     """Return a greatest common divisor of two numbers."""
     while num1 != 0 and num2 != 0:
         if num1 > num2:
             num1 = num1 % num2
         elif num1 == num2:
             gcd = num1
-            return str(gcd)
+            return gcd
         else:
             num2 = num2 % num1
     gcd = num1 + num2
@@ -23,6 +23,6 @@ def generate_round():
     in the brain-game engine."""
     num1, num2 = random.randrange(1, 51, 3), random.randrange(1, 51, 3)
     question = str(num1) + ' ' + str(num2)
-    answer = str(define_gcd(num1, num2))
+    answer = str(calculate_gcd(num1, num2))
     return (question,
             answer)
